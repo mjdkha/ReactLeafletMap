b0543907e63df4a548e13b02bd089d2c13930f98
hamzaboulman <hamzaboulman132@gmail.com>
2022-04-06 15:46:55 +0000

    fix bugs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LatLng, LeafletView, WebViewLeafletEvents } from 'react-native-leaflet-view';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <LeafletView
+  mapLayers={[
+    {
+      baseLayerName: "MapTiler",
+      baseLayerIsChecked: true,
+      baseLayer: true,
+      url: "https://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
+    },
+  ]}
+  />
   );
 }
 
