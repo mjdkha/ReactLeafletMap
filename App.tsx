83f66e10dc95be78c1f515cbed0dcd6124b7add5
user <user@user.com>
2022-04-07 09:21:36 +0000

    add map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import MapView,{ Marker } from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  });
+
+
+  useEffect(() => {
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      });
+    })();
+
+  }, []);
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={location}
+      >
+<Marker coordinate={location} />
+      </MapView>
+      
     </View>
   );
 }
@@ -13,8 +48,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
