9c0b69294303a6b8a455140357dc2e29619a18fc
user <user@user.com>
2022-04-07 12:02:31 +0000

    add map

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import MapView,{ Marker } from 'react-native-maps';
+import { Alert, Button, Modal, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import MapView,{ Callout, MapEvent, Marker } from 'react-native-maps';
 import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
+import { Popup } from 'react-native-map-link';
+
 
 export default function App() {
   const [location, setLocation] = useState({
@@ -11,6 +13,18 @@ export default function App() {
     longitudeDelta: 0,
   });
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [userInput , setUserInput] = useState("")
+  const [marker , setMarker] = useState([] as any)
+  const onLocationSelect = (event: MapEvent) => {
+    setLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    });
+  }
+  
 
   useEffect(() => {
 
@@ -37,11 +51,51 @@ export default function App() {
       <MapView
         style={{ alignSelf: 'stretch', height: '100%' }}
         region={location}
+        onPress={onLocationSelect}
       >
-<Marker coordinate={location} />
+    
+    <Marker coordinate={location} >
+
+<Callout style={{ alignSelf: 'stretch', height: '100%' }}>
+<Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+            
+      </Pressable>
+</Callout>
+      </Marker>
+
+      <Callout>
+        
+      </Callout>
       </MapView>
       
     </View>
+
+
   );
 }
 
@@ -49,5 +103,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
 
